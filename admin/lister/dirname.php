c266df6b4055e48f3addd7000071a39a903718fa
Saalek110 <160769553+Saalek110@users.noreply.github.com>
2024-11-23 04:53:15 +0330

    Delete admin directory

--- admin/lister/dirname.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo dirname('.');    // Will return '.'.
-echo "<br>".dirname('/');    // Will return `\` on Windows and '/' on *nix systems.
-echo "<br>".dirname('\\');   // Will return `\` on Windows and '.' on *nix systems.
-echo "<br>".dirname('C:\\'); // Will return 'C:\' on Windows and '.' on *nix systems.
-?>
--- admin/test/my_server.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo $_SERVER['SERVER_NAME'];
-
-
-echo "<br>".$_SERVER['PHP_SELF'] ;
-?>
